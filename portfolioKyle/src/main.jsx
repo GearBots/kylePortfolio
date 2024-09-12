5a12215d34a86d286c5bb3d73f38b3a80f7b86f5
Kyle Geary <142063304+GearBots@users.noreply.github.com>
2024-09-12 09:10:20 -0600

    inital file structure

--- portfolioKyle/src/main.jsx
+++ portfolioKyle/src/main.jsx
@@ -2,9 +2,19 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import About from './components/About.jsx'
+import Contact from './components/Contact.jsx'
+import Landing from './components/Landing.jsx'
+import Projects from './components/Projects.jsx'
+import Experience from './components/Experience.jsx' 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <Landing />
+    <About />
+    <Experience />
+    <Projects />
+    <Contact />
     <App />
   </StrictMode>,
 )
